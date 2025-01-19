02cad2c7e2e049922bf6136add7a1ec5719f7439
Shivam Thapliyal <shivamthapliyal571@gmail.com>
2025-01-19 20:48:38 +0530

    Update Result.jsx

--- frontend/src/component/Result.jsx
+++ frontend/src/component/Result.jsx
@@ -30,7 +30,7 @@ function Result() {
   useEffect(() => {
     const fetchAnalysis = async () => {
       try {
-        const response = await fetch("https://assignment-anchors-frontend.vercel.app/api/comments", {
+        const response = await fetch("https://assignment-anchors-backend.vercel.app/api/comments", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ videoUrl }),
