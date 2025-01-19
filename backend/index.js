100b180b4b1381e4e45db9128589347f0cc29cc9
Shivam Thapliyal <shivamthapliyal571@gmail.com>
2025-01-19 20:43:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,8 @@ const PORT = 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
-    methods: ["GET", "POST"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://assignment-anchors-frontend.vercel.app"],
+    methods: ["GET", "POST", "OPTIONS"],
   })
 );
 app.use(express.json());
